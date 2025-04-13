903a0ccb08eade3a1e6b0c32710f5ef689457f3c
melikewaffles <adrian11227waffle@gmail.com>
2025-04-13 10:01:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const button = document.getElementById("bigRedButton");
 const sound = document.getElementById("buttonSound");
 
-button.addEventListener("mouseup", () => {
-  sound.currentTime = 0;
-  sound.play();
-});
+function pressButton() {
+  button.classList.add("pressed");
+}
 
-button.addEventListener("touchend", () => {
+function releaseButton() {
+  button.classList.remove("pressed");
   sound.currentTime = 0;
   sound.play();
-});
+}
+
+button.addEventListener("mousedown", pressButton);
+button.addEventListener("mouseup", releaseButton);
+button.addEventListener("mouseleave", () => button.classList.remove("pressed"));
+
+// Touch support for mobile
+button.addEventListener("touchstart", pressButton);
+button.addEventListener("touchend", releaseButton);
